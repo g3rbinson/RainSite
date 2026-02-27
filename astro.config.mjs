768817af687cd56c3bca47a640d97ec45cb80b26
g3rbinson <g3rbinson@gmail.com>
2026-02-26 19:56:50 -0600

    Fix trailing slash on base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://g3rbinson.github.io',
-  base: '/RainSite',
+  base: '/RainSite/',
   build: {
     assets: '_assets',
   },
